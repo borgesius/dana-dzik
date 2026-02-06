4131b7683523a75b3cc2551cff7fb26fecb28ba2
Dana Dzik <50225082+borgesius@users.noreply.github.com>
2026-02-06 12:48:21 -0800

    feat: expose compu97 os details

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ import { createAudioManager } from "./lib/audio"
 import { GlitchManager } from "./lib/glitchEffects"
 import { isMobile } from "./lib/isMobile"
 import { Router } from "./lib/router"
+import { SystemCrashHandler } from "./lib/systemCrash"
 
 setupErrorHandlers()
 trackPageview()
@@ -66,6 +67,7 @@ function initMobile(app: HTMLElement): void {
 
     createAudioManager()
     new GlitchManager()
+    new SystemCrashHandler()
 
     document.addEventListener("click", (e) => {
         const target = e.target as HTMLElement
@@ -101,6 +103,7 @@ function initDesktop(app: HTMLElement): void {
     new CursorTrail()
     createAudioManager()
     new GlitchManager()
+    new SystemCrashHandler()
     new Widgets(app)
 
     windowManager.onNewWindowOpen(() => {
